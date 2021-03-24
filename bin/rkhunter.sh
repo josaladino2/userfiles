0567bf80187a34529761730263eef73713ec30cf
Joe Saladino <joe@bram.net>
2021-03-23 20:38:38 -0600

    deleted files

--- bin/rkhunter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-/usr/local/bin/rkhunter --update --nocolors
-/usr/local/bin/rkhunter --propupd --nocolors
-#/usr/local/bin/rkhunter -c --sk --disable apps,suspscan,deleted_files --rwo
-if [ -t 0 ]; then
-	/usr/local/bin/rkhunter -c --sk --rwo --nocolors
-else
-	/usr/local/bin/rkhunter -c --sk
-fi
